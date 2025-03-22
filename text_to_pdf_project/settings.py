b3ece9039c3796274cb80ac2cfb5173fa9591568
Shanu sinha <118159555+iamshanusinha@users.noreply.github.com>
2025-03-22 18:44:34 +0530

    updated

--- text_to_pdf_project/settings.py
+++ text_to_pdf_project/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'text_to_pdf_app',  # your custom app
+    'text_to_pdf_app',
 ]
 
 MIDDLEWARE = [
